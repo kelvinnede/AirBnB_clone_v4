06e29c992a835779c260393bc49dd3546b96cc0a
kelvinnede <kelvinnede@gmail.com>
2024-05-18 13:19:33 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,17 +5,20 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown."""
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a list of all State objects present in DBStorage sorted by name."""
+    """Displays a list of all State objects in DBStorage sorted by name."""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
